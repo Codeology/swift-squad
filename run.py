453d73cc8a192e41944713e9e0a86e4ae3b5da8c
Alicia Matsumoto <aliciamatsumoto@berkeley.edu>
2020-11-04 21:24:24 -0800

    skeleton for run file

--- run.py
+++ run.py
@@ -2,3 +2,17 @@
 VSCOdeology backend, Fall 2020
 Contributors: Ashley Chu, Alicia Matsumoto
 """
+
+from flask import Flask, send_file
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# class Class(Resource):
+    # method
+
+# api.add_resource(class, '/endpoint')
+
+if __name__ == '__main__':
+    app.run(debug=True)
