67d40b68358a1f2c03dd0867eebf645a1a41737e
Ashley Chu <ashchu890@gmail.com>
2020-11-07 16:54:25 -0800

    Adding in run.py API template

--- run.py
+++ run.py
@@ -2,17 +2,27 @@
 VSCOdeology backend, Fall 2020
 Contributors: Ashley Chu, Alicia Matsumoto
 """
-
 from flask import Flask, send_file
 from flask_restful import Resource, Api
+import cluster as cl
 
 app = Flask(__name__)
 api = Api(app)
 
-# class Class(Resource):
-    # method
 
-# api.add_resource(class, '/endpoint')
+# ImageApi to process image
+class ImageApi(Resource):
+
+    # User requests
+    def get(self):
+        return "GET passed"
+
+    def post(self):
+        cl.test()
+        return "yay POST passed :)"
+
+
+api.add_resource(ImageApi, '/endpoint')
 
 if __name__ == '__main__':
     app.run(debug=True)
