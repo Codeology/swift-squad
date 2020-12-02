9473681e67d351d425eb4061ac4899ca6ed4ac8e
Alicia Matsumoto <56315176+aliciamatsumoto@users.noreply.github.com>
2020-12-01 20:40:26 -0800

    file processing  (#3)
    * update README
    
    * imports
    
    * add clustering
    
    * image received
    
    * successful readings for ali
    
    * remove saving logic
    
    * remove idea
    
    * cite blog post

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ VSCOdeology backend, Fall 2020
 Contributors: Ashley Chu, Alicia Matsumoto
 """
 
-from flask import Flask, send_file
-from flask_restful import Resource, Api
+import werkzeug
+from flask import Flask, send_file, request
+from flask_restful import Resource, Api, reqparse
 import cluster as cl
 import cv2
 import numpy as np
 from sklearn.cluster import KMeans
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,31 +19,47 @@ api = Api(app)
 # ImageApi to process image
 class ImageApi(Resource):
 
-    # User requests
-    def get(self):
-        return "GET passed"
+    def __init__(self):
+        # Create a request parser
+        parser = reqparse.RequestParser()
+        parser.add_argument("image", type=werkzeug.datastructures.FileStorage, location='files')
+        # Sending more info in the form? simply add additional arguments, with the location being 'form'
+        # parser.add_argument("other_arg", type=str, location='form')
+        self.req_parser = parser
+
+
+    # # User requests (for easy testing)
+    # def get(self):
+    #     return "GET passed"
 
     # MAIN METHOD:
-    def post(self, image_file):
-        img = cv2.imread(image_file) # this will probably need to be changed to be compatible
+    def post(self):
+        # if you change the name 'image' in .get() here, you must also change it on the frontend:
+        image_file = self.req_parser.parse_args(strict=True).get("image", None)
+        if image_file:
+            # Get the byte content using `.read()`
+            image = image_file.read()
 
-        img_copy = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            np_arr = np.fromstring(image, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-        # Since the K-means algorithm we're about to do,
-        # is very labour intensive, we will do it on a smaller image copy
-        # This will not affect the quality of the algorithm
-        pixelImage = img.reshape((img.shape[0] * img.shape[1], 3))
-        # We use the sklearn K-Means algorithm to find the color histogram
-        # from our small size image copy
-        clt = KMeans(n_clusters=9)
-        clt.fit(pixelImage)
+            # Since the K-means algorithm we're about to do,
+            # is very labour intensive, we will do it on a smaller image copy
+            # This will not affect the quality of the algorithm
+            pixelImage = img.reshape((img.shape[0] * img.shape[1], 3))
+            # We use the sklearn K-Means algorithm to find the color histogram
+            # from our small size image copy
+            clt = KMeans(n_clusters=9)
+            clt.fit(pixelImage)
 
-        hist = cl.centroid_histogram(clt)
-        color_data = np.array(clt.cluster_centers_, dtype=int)
-        colors = cl.convert_to_hex(color_data)
-        colors = np.sort(colors)[::-1]
-        print(colors[1:])
-        return "yay POST passed :)"
+            hist = cl.centroid_histogram(clt)
+            color_data = np.array(clt.cluster_centers_, dtype=int)
+            colors = cl.convert_to_hex(color_data)
+            colors = np.sort(colors)[::-1]
+            print(colors[1:])
+            return {"colors": [color for color in colors]}
+        else:
+            return "No image sent :("
 
 
 api.add_resource(ImageApi, '/endpoint')
